88d86a32312babcb7194a09dc95fd8e09fd533cb
CaioDGeraldi <chdgeraldi@gmail.com>
2026-04-07 20:51:04 -0300

    CRUD tasks

--- task/urls.py
+++ task/urls.py
@@ -4,7 +4,7 @@ from .views import index, task_list, create_task, edit_task, delete_task
 urlpatterns = [
     path('', index, name='index'),
     path('tasks/', task_list, name='tasks'),
-    path('create_task/', create_task, name='create_task'),
-    path('edit_task/', edit_task, name='edit_task'),
-    path('delete_task/', delete_task, name='delete_task'),
+    path('create/', create_task, name='create'),
+    path('edit/<int:id>', edit_task, name='edit'),
+    path('delete/<int:id>', delete_task, name='delete'),
 ]
--- task/views.py
+++ task/views.py
@@ -20,8 +20,20 @@ def create_task(request):
     
     return render(request, 'task/create_task.html')
 
-def delete_task(request):
-    return render(request, 'task/delete_task.html')
+def delete_task(request, id):
+    Tarefa.objects.get(id=id).delete()
+    return redirect('tasks')
+     
 
-def edit_task(request):
-    return render(request, 'task/edit_task.html')
+def edit_task(request, id):
+    tarefa = Tarefa.objects.get(id=id)
+
+    if request.method == 'POST':
+        tarefa.titulo = request.POST.get('titulo')
+        tarefa.descricao = request.POST.get('descricao')
+        tarefa.data = request.POST.get('data')
+        tarefa.status = 'status' in request.POST
+        tarefa.save()
+        return redirect('tasks')
+    
+    return redirect('tasks')
