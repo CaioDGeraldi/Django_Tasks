91abef2db0f0ab0e7e7b683023c2fb593c1276c4
CaioDGeraldi <chdgeraldi@gmail.com>
2026-04-14 20:29:07 -0300

    :feat: login e auth

--- task/urls.py
+++ task/urls.py
@@ -1,10 +1,10 @@
-from django.urls import path
-from .views import index, task_list, create_task, edit_task, delete_task
+from django.urls import path, include
+from .views import task_list, create_task, edit_task, delete_task
 
 urlpatterns = [
-    path('', index, name='index'),
-    path('tasks/', task_list, name='tasks'),
+    path('', task_list, name='index'),
     path('create/', create_task, name='create'),
     path('edit/<int:id>', edit_task, name='edit'),
     path('delete/<int:id>', delete_task, name='delete'),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
--- task/views.py
+++ task/views.py
@@ -1,39 +1,37 @@
 from django.shortcuts import render,redirect
 from task.models import Tarefa
+from task.forms import TarefaForm
+from django.contrib.auth.decorators import login_required
 
-def index(request):
-    return render(request, 'task/index.html')
-
+@login_required
 def task_list(request):
     tarefas = Tarefa.objects.all()
     return render(request, 'task/tasks.html', {'tarefas':tarefas})
 
+@login_required
 def create_task(request):
+    form = TarefaForm(request.POST or None)
     if request.method == 'POST':
-        titulo = request.POST.get('titulo')
-        data = request.POST.get('data')
-        descricao = request.POST.get('descricao')
-        status = False
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+        
+    return render(request, 'task/create_task.html', {'tarefa':form})
 
-        Tarefa.objects.create(titulo=titulo, descricao=descricao, data=data, status=status)
-        return redirect('tasks')
-    
-    return render(request, 'task/create_task.html')
-
+@login_required
 def delete_task(request, id):
     Tarefa.objects.get(id=id).delete()
-    return redirect('tasks')
+    return redirect('index')
      
-
+@login_required
 def edit_task(request, id):
     tarefa = Tarefa.objects.get(id=id)
-
     if request.method == 'POST':
         tarefa.titulo = request.POST.get('titulo')
         tarefa.descricao = request.POST.get('descricao')
         tarefa.data = request.POST.get('data')
         tarefa.status = 'status' in request.POST
         tarefa.save()
-        return redirect('tasks')
+        return redirect('index')
     
-    return redirect('tasks')
+    return redirect('index')
