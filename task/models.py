991e796ca7b19b99e5c4a7d2e9b876d75a63c5ad
CaioDGeraldi <chdgeraldi@gmail.com>
2026-04-28 19:53:27 -0300

    :sparkles: feat: DELETE ON CASCADE para Usuários

--- task/models.py
+++ task/models.py
@@ -1,22 +1,26 @@
 from django.db import models
+from django.conf import settings 
 
 class Tarefa(models.Model):
-
+    
     titulo = models.CharField(
-        max_length=30,
-        null=False,
-        blank=False
+        max_length=30, null=False, blank=False 
     )
-
+    
     descricao = models.CharField(
         max_length=500,
         null=False,
-        blank=False
-    )
-
+        blank=False 
+    ) 
+    
     data = models.DateField()
-
-    status = models.BooleanField()
-
-    def __str__(self):
-        return self.titulo
+    
+    status = models.BooleanField() 
+    
+    usuario = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete= models.CASCADE,
+        related_name='tarefas',
+    )
+    
+    def __str__(self): return self.titulo
